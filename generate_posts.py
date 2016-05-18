9a22308cfd3746281818d59ab0dee9da39458a5d
ecresswell <ecresswell@gmail.com>
2016-05-18 22:02:04 +0100

    TFA - Separate news and reports.

--- generate_posts.py
+++ generate_posts.py
@@ -3,13 +3,27 @@ import re
 import urllib2
 import datetime
 import os
+import sys
 
 input_xml = 'http://www.tauntonfestival.org.uk/sitemap-posttype-post.xml'
-output_dir = r'C:/myprojects/tauntonfestival/_news'
+news_output_dir = r'C:/myprojects/tauntonfestival/_news'
+reports_output_dir = r'C:/myprojects/tauntonfestival/_reports'
 
-template = """---
-layout: page
+news_template = """---
+layout: news
 title: $TITLE
+date: $DATE
+redirect_from: "/$URL"
+---
+<section>
+$CONTENT
+</section>
+"""
+
+report_template = """---
+layout: report
+title: $TITLE
+tags: $TAGS
 date: $DATE
 redirect_from: "/$URL"
 ---
@@ -23,11 +37,32 @@ def get_post_info(url):
     html = response.read()
     title = re.findall(r'<h2 class="page-title">(.*?)</h2>', html)
     date = re.findall(r'<section class="metaline">Published (.*?) by .*?</section>', html)
+    tags = get_tags(html)
     content = re.findall(r'<section class="entry single">(.*?)</section>', html, re.S)
-    return (title, date, content)
+    return (title, date, content, tags)
+    
+def get_tags(html):
+    result = []
+    tags_temp = re.findall(r'<section class="metaline">.* in (.*?)</section>', html)
+    return re.findall(r'rel="category tag">(.*?)</a>', tags_temp[0])
     
 def generate_news_page(rel_url, title, date, content):
-    return template.replace('$TITLE', title).replace('$URL', rel_url).replace('$DATE', date.strftime('%Y-%m-%d 09:00:00')).replace('$CONTENT', content)
+    content = fix_links_in_content(content)
+    return news_template.replace('$TITLE', title).replace('$URL', rel_url).replace('$DATE', date.strftime('%Y-%m-%d 09:00:00')).replace('$CONTENT', content)
+
+def generate_report_page(rel_url, title, date, content, tags):
+    content = fix_links_in_content(content)
+    tag_string = '\n - '.join([''] + tags)
+    return report_template.replace('$TITLE', title).replace('$URL', rel_url).replace('$DATE', date.strftime('%Y-%m-%d 09:00:00')).replace('$CONTENT', content).replace('$TAGS', tag_string)
+    
+def fix_links_in_content(content):
+    # href="/2014/03/annual-general-meeting-26th-march-2014/"
+    # href="http://www.tauntonfestival.org.uk/wp-content/uploads/2014/09/TauntonFestival_2015_Syllabus.pdf"
+    # onclick="_gaq.push(['_trackEvent','download','http://www.tauntonfestival.org.uk/wp-content/uploads/2014/09/TauntonFestival_2015_Syllabus.pdf']);"
+    content = re.sub(r'href="/(.*?)"', r'href="{{ "/\1" | prepend: site.github.url }}"', content)
+    content = re.sub(r'href="http://www.tauntonfestival.org.uk(.*?)"', r'href="{{ "\1" | prepend: site.github.url }}"', content)
+    content = re.sub(r' onclick="_gaq.*?;"', r'', content)
+    return content
     
 def relative_url(url):
     return url.replace('http://www.tauntonfestival.org.uk/', '')
@@ -42,14 +77,23 @@ def main():
     posts = re.findall(r"<loc>(.*?)</loc>", html)
     
     for post in posts:
-        (title, date, content) = get_post_info(post)
+        (title, date, content, tags) = get_post_info(post)
         rel_url = relative_url(post)
         parsed_date = datetime.datetime.strptime(date[0], '%B %d, %Y')
         happy_title = title[0].replace(':', '-')
-        post_md = generate_news_page(rel_url, happy_title, parsed_date, content[0])
         page_name = get_page_name(rel_url, parsed_date)
-        page = output_dir + '/' + page_name
+        
+        if len(tags) is 1 and 'News' in tags:
+            post_md = generate_news_page(rel_url, happy_title, parsed_date, content[0])
+            page = news_output_dir + '/' + page_name
+        elif 'News' in tags:
+            raise ValueError('Found post that is news & other stuff - ' + page_name)
+        else:
+            post_md = generate_report_page(rel_url, happy_title, parsed_date, content[0], tags)
+            page = reports_output_dir + '/' + page_name
+
         print page
+        sys.stdout.flush()
         with open(page, "w") as file:
             file.write(post_md)
         #print page_name
